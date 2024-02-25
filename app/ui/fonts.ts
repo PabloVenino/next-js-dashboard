413e7673edb9fccacb7824646502cf0a442dc3e8
Pablo Venino <pablo.venino@mult-e.com.br>
2024-02-24 23:32:25 -0300

    alterações do dashboard e page navigation

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+
+import { Fredoka, Lusitana, Ubuntu } from 'next/font/google';
+
+export const fredoka = Fredoka({ subsets: ['latin'] });
+export const ubunto = Ubuntu({ weight: "400", subsets: ['latin'] })
+export const lusitana = Lusitana({ weight: "400", subsets: ['latin'] })
